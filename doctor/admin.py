390c99ff1818effd7cd5305d23cbbd88d7347ff6
UhaiMong <uhaimarma2016@gmail.com>
2024-08-12 00:40:40 +0600

    upload

---
+++ doctor/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from . import models
+# Register your models here.
+
+admin.site.register(models.AvailableTime)
+class DesignationModelAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ["name",]}
+admin.site.register(models.Designation,DesignationModelAdmin)
+class SpecializationModelAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ["name",]}
+admin.site.register(models.Specialization,SpecializationModelAdmin)
+admin.site.register(models.Doctor)
