41c3a7d9d74bc0eeb033c7b5f26cecc796afeb7a
UhaiMong <uhaimarma2016@gmail.com>
2024-08-15 14:03:11 +0600

    specific doctor filtering

--- doctor/views.py
+++ doctor/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets,filters,pagination
 from .import serializers
 from .import models
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 # Create your views here.
 
+
+
+class DoctorPagination(pagination.PageNumberPagination):
+    page_size = 1 # item per page
+    page_size_query_param =  page_size
+    max_page_size = 100
 class DoctorViewSet(viewsets.ModelViewSet):
     queryset = models.Doctor.objects.all()
     serializer_class = serializers.DoctorSerializer
+    filter_backends = [filters.SearchFilter]
+    pagination_class = DoctorPagination
 
 
 class DesignationViewSet(viewsets.ModelViewSet):
@@ -21,11 +29,19 @@ class SpecializationViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.SpecializationSerializer
 
 
+class AvailableTimeForSpecificDoctor(filters.BaseFilterBackend):
+    def filter_queryset(self,request,query_set,view):
+        doctor_id = request.query_params.get("doctor_id")
+        if doctor_id:
+            return query_set.filter(doctor=doctor_id)
+        return query_set
 class AvailableTimeViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = models.AvailableTime.objects.all()
     serializer_class = serializers.AvailableTimeSerializer
+    filter_backends=[AvailableTimeForSpecificDoctor]
 
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = models.Review.objects.all()
     serializer_class = serializers.ReviewSerializer
+    
